f1e979e63e73653cb737e642648a565bad2dfc2c
miguepoloc <miguepoloc@gmail.com>
2022-07-02 17:44:50 -0500

    Corrección de mes y añadiendo hora a los resultados

--- src/components/Dashboard/mod_autoevaluativo/Resultado.jsx
+++ src/components/Dashboard/mod_autoevaluativo/Resultado.jsx
@@ -5,8 +5,12 @@ export const Resultado = ({ number, resultado, setRenderResultados, surveys }) =
     let encuestaConRespuestas = surveys.loadDataAnswerUser(resultado.respuestas)
     var fecha = new Date(resultado.fecha);
     let dia = fecha.getDate();
-    let mes = fecha.getMonth();
+    let mes = fecha.getMonth() + 1;
     let anio = fecha.getFullYear();
+    let hora = fecha.getHours();
+    let minutos = fecha.getMinutes();
+    let segundos = fecha.getSeconds();
+
     return (
         <div className="" >
             {/* Validaciones que deberían ir en reusltados.creo. */}
@@ -20,7 +24,11 @@ export const Resultado = ({ number, resultado, setRenderResultados, surveys }) =
                             }}>
                             <div className="text-center pb-0 d-flex flex-column align-items-center justify-content-center">
                                 <p className='display-1  m-0 text-white'><strong>{number}</strong></p>
-                                <p className='m-0 text-white'>{`${dia<10?"0"+dia:dia}/${mes<10?"0"+mes:mes}/${anio}`}</p>
+                                <p className='m-0 text-white'>
+                                    {`${dia < 10 ? "0" + dia : dia}/${mes < 10 ? "0" + mes : mes}/${anio}`}
+                                    <br />
+                                    {`${hora < 10 ? "0" + hora : hora}:${minutos < 10 ? "0" + minutos : minutos}:${segundos < 10 ? "0" + segundos : segundos}`}
+                                </p>
                             </div>
                         </div>
                     }
