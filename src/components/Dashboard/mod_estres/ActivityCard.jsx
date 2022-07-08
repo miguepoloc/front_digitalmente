54418011037cbd9d24f0f1e8f820923d68bdbbe8
Daniel Vega <42049035+dan077@users.noreply.github.com>
2022-07-08 01:09:19 -0500

    seccion inicial "termianada"

--- src/components/Dashboard/mod_estres/ActivityCard.jsx
+++ src/components/Dashboard/mod_estres/ActivityCard.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-
+import { FaLeaf } from 'react-icons/fa'
 export const ActivityCard = ({title,desc,_class}) => {
 
   return (
     <div class={"activityCard-card d-flex flex-column " + (_class? _class:"")}>
       <div class="activityCard-card-header ">
-                        <svg viewBox="0 -13 512 512" xmlns="http://www.w3.org/2000/svg" style={{backgroundColor:"#2e2882"}}>
-                            <g fill="#feb0a5">
-                                <path d="M256 92.5l127.7 91.6L512 92 383.7 0 256 91.5 128.3 0 0 92l128.3 92zm0 0M256 275.9l-127.7-91.5L0 276.4l128.3 92L256 277l127.7 91.5 128.3-92-128.3-92zm0 0" />
-                                <path d="M127.7 394.1l128.4 92 128.3-92-128.3-92zm0 0" />
-                            </g>
-                            <path d="M512 92L383.7 0 256 91.5v1l127.7 91.6zm0 0M512 276.4l-128.3-92L256 275.9v1l127.7 91.5zm0 0M256 486.1l128.4-92-128.3-92zm0 0" fill="#feb0a5" />
-                        </svg>
+                        <div style={{backgroundColor:"#F5F7FE"}} className="rounded-1"><FaLeaf className='p-1' size={35} fill={"#B2C7FC"}/></div>
                         <div class="menu-dot"></div>
                     </div>
     <div class="activityCard-card-title">{title}</div>
