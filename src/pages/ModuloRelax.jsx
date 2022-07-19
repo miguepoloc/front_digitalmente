a1260dd17defa83f16971c426c1c10befa2a89ae
miguepoloc <miguepoloc@gmail.com>
2022-07-19 00:59:54 -0500

    Referencias corregidas del módulo Estrés

--- src/pages/ModuloRelax.jsx
+++ src/pages/ModuloRelax.jsx
@@ -151,33 +151,34 @@ const ModuloRelax = () => {
                         <FooterDashboard />
                         <ButtonLibro text={`
                         <ol>
-                        <li>	Ávila, J. (2014). El estrés un problema de salud del mundo actual. Revista Con-Ciencia. 2(1), 117-125.	</li>	<br/>
-<li>	Aguilar, G., & Musso, A. (2008). La meditación como proceso cognitivo-conductual. Suma Psicológica. 15(1), 241-258.	</li>	<br/>
-<li>	Daza, F. (1992). Prevención del estrés: intervención sobre el individuo. Guías de buenas prácticas. NTP 349: Prevención del estrés: intervención sobre el individuo (saludlaboral.org)	</li>	<br/>
-<li>	El Espectador. (2020-2022). Meditación guiada para reducir el estrés [Pódcast]. Meditación guiada para reducir el estrés - Shala | Podcast en Spotify	</li>	<br/>
-<li>	Guo, Y., Wang, S., & Johnson, V. (2011). College student's stress under current economic downturn [El estrés de los estudiantes universitarios en la recesión económica actual]. Revista College Student Journal, 45 (3), 536-543.	</li>	<br/>
-<li>	Godoy, D., Eberhard, A., Abarca, F., Acuña, B., & Muñoz, R. (2020). Psicoeducación en salud mental: una herramienta para pacientes y familiares. Revista Médica Clínica Las Condes. 31(2), 169-173. 	</li>	<br/>
-<li>	Hernández, M. R. (2011). Caminar 10000 pasos al día para mantener una buena salud y calidad de vida. InterSedes. 12(24).	</li>	<br/>
-<li>	Jakovcevic, A., Franco, P., Dalla Pozza, M. V., & Ledesma, R. (2016). Percepción de los beneficios individuales del uso de la bicicleta compartida como modo de transporte. suma psicológica, 23(1), 33-41.	</li>	<br/>
-<li>	Lazarus, R. (2000). Estrés y emoción. Manejo e implicaciones en nuestra salud. Editorial Desclée Brouwer.	</li>	<br/>
-<li>	Lisboa, C., & Spadoti, R. (2004). Estressores em uma unidade pós-operatória de cirugia torácica: Avaliaçao da enfermagem [Estresores en una unidad de cirugía postoperatoria Torácico: orácico: evaluación de enfermería]. Revista Latino-Americana de Enfermagem, 12 (1), 22-27. 	</li>	<br/>
-<li>	Lumowell-Fitness-ES. (2016, 2 de diciembre). Ejercicios para principiantes en casa [Video]. You tube https://www.youtube.com/watch?v=wXC2EXuXoIc	</li>	<br/>
-<li>	Mosconi, S., Correche, M., Rivarola, M., & Penna, F. (2007). Aplicación de la técnica de relajación en deportistas para mejorar su rendimiento. Fundamentos en humanidades. (16), 183-198.	</li>	<br/>
-<li>	Muñoz, T. (2009-2022). Meditación guiada: Reducir el estrés por la respiración [Pódcast]. Spotify Spotify – Meditación guiada: Reducir el estrés por la respiración - Pad - song by Teresa Muñoz	</li>	<br/>
-<li>	Nezu, A., Maguth ,C., & D’Zurilla, T. (2014). Terapia de solución de problemas. Editorial Desclée de Brouwer.	</li>	<br/>
-<li>	Namalyongo, A; Achón, Z; & Ábalo, R. (2013). Factores de riesgo y vulnerabilidad al estrés en estudiantes universitarios. Psicogente. 16(29), 143-154.	</li>	<br/>
-<li>	Oman, D., Shapiro, S. L., Thoresen, C. E., Plante, T. G., y Flinders, T. (2008). Meditation lowers stress and supports forgiveness among college students: A randomized controlled trial. Journal of American College Health. 56(5), 569-578	</li>	<br/>
-<li>	Pereira, M. (2009). Una revisión teórica sobre el estrés y algunos aspectos relevantes de éste en el ámbito educativo. Revista educación, 33(2), 171-190.	</li>	<br/>
-<li>	Pérez, J., García, R., & Pérez F. (s.f.). Guía para el mármol del estrés académico. estres.pdf (uv.es)	</li>	<br/>
-<li>	Santos, A., Fernández, M., Reig, A., Riquelme, L., Montero, E., & Peralta, M. (2019). Valoración del estrés percibido y de las necesidades y demandas de intervención psicoeducativa para su manejo eficaz en estudiantes universitarios.[DAC2] 	</li>	<br/>
-<li>	Shapiro, S.L., Brown, K.W., y Biegel, G.M. (2007). Teaching self-care to caregivers: Effects of mindfulness based stress reduction on the mental health of therapists in training. Training and Education in Professional Psychology, 1(2), 105-115.	</li>	<br/>
-<li>	Tenelanda, D. Castelo, M. Cando, I.  Fierro, S & Asqui, J. (2017). Las técnicas de respiración, relajación y visualización para el manejo del estrés académico en el proceso de aprendizaje del idioma inglés. In 8va Edición de la Conferencia Científica Internacional de la Universidad de Holguín.[DAC1] 	</li>	<br/>
-<li>	Treviño, J., & Ramírez, M. (2012). Estrategias cognitivo-conductuales para el manejo del estrés en alumnos mexicanos de bachillerato internacional. Alternativas en psicología. 16(26), 26-38.	</li>	<br/>
-<li>	Valenzuela, M., Gallegos, L., Baca, L., López, H., & Rico, F. (2021). Estrés académico en universitarios y la práctica de ejercicio físico-deportivo. Revista Publicando, 8(28), 1-8.	</li>	<br/>
-<li>	Valverde, C., Crespo, M., & Ring, J. (2003). Estrategias de afrontamiento.  Medicina Clínica en Psiquiatría y Psicología.[DAC3] 	</li>	<br/>
-<li>	Villen, J., 2016. Técnicas cognitivo-conductuales para afrontar el estrés de los exámenes. Técnicas cognitivo-conductuales para afrontar el estrés de los exámenes (elsevier.com)	</li>	<br/>
-<li>	Villarroel, A. (2015). Intervención cognitivo-conductual y centrada en soluciones para disminuir el estrés académico en estudiantes universitarios. [Tesis maestría, Universidad Autónoma de Nuevo León]. http://eprints.uanl.mx/9644/1/1080215011.pdf	</li>	<br/>
-<li>	Zuazua, A. L. V., & Ramírez, M. T. G. (2017). Intervención cognitivo-conductual y centrada en soluciones para disminuir el estrés académico en estudiantes universitarios. Revista Electrónica de Psicología Iztacala. 18(4), 1363-1387.	</li>	<br/>
+                        <li>	Aguilar, G. y Musso, A. (2008). La meditación como proceso cognitivo-conductual. Suma Psicológica, 15(1), 241-258.	</li> 	<br/>
+<li>	Ávila, J. (2014). El estrés un problema de salud del mundo actual. Revista Con-Ciencia, 2(1), 117-125. http://farbio.edu.bo/csegc/conciencia/index.php/ojs/article/view/110 	</li> 	<br/>
+<li>	Daza, F. (1992). Prevención del estrés: intervención sobre el individuo (Guías de buenas prácticas, NTP 349). https://www.insst.es/documents/94886/326853/ntp_349.pdf 	</li> 	<br/>
+<li>	El Espectador. (Productor). (2020-presente). Meditación guiada para reducir el estrés [Podcast de audio]. Spotify. https://open.spotify.com/show/2TWGZoOiMS9jchA5vqSVL3 	</li> 	<br/>
+<li>	Godoy, D., Eberhard, A., Abarca, F., Acuña, B. y Muñoz, R. (2020). Psicoeducación en salud mental: una herramienta para pacientes y familiares. Revista Médica Clínica Las Condes, 31(2), 169-173. https://doi.org/10.1016/j.rmclc.2020.01.005 	</li> 	<br/>
+<li>	Guo, Y., Wang, S. y Johnson, V. (2011). College student's stress under current economic downturn [El estrés de los estudiantes universitarios en la recesión económica actual]. Revista College Student Journal, 45(3), 536-543.	</li> 	<br/>
+<li>	Hernández, M. R. (2011). Caminar 10000 pasos al día para mantener una buena salud y calidad de vida. InterSedes, 12(24), 137-145. https://doi.org/10.15517/isucr.v12i24.969 	</li> 	<br/>
+<li>	Jakovcevic, A., Franco, P., Dalla Pozza, M. V. y Ledesma, R. (2016). Percepción de los beneficios individuales del uso de la bicicleta compartida como modo de transporte. Suma Psicológica, 23(1), 33-41.	</li> 	<br/>
+<li>	Lazarus, R. (2000). Estrés y emoción. Manejo e implicaciones en nuestra salud. Editorial Desclée Brouwer.	</li> 	<br/>
+<li>	Lisboa, C. y Spadoti, R. (2004). Estressores em uma unidade pós-operatória de cirugia torácica: Avaliaçao da enfermagem [Estresores en una unidad de cirugía postoperatoria Torácico: orácico: evaluación de enfermería]. Revista Latino-Americana de Enfermagem, 12(1), 22-27.	</li> 	<br/>
+<li>	Lumowell-Fitness-ES. (2016, 2 de diciembre). Ejercicios para principiantes en casa [Video]. YouTube https://www.youtube.com/watch?v=wXC2EXuXoIc 	</li> 	<br/>
+<li>	Mosconi, S., Correche, M., Rivarola, M. y Penna, F. (2007). Aplicación de la técnica de relajación en deportistas para mejorar su rendimiento. Fundamentos en Humanidades, (16), 183-198.	</li> 	<br/>
+<li>	Muñoz, T. (Anfitrión) (2009-presente). Meditación guiada: Reducir el estrés por la respiración [Podcast de audio]. Spotify. https://open.spotify.com/album/6VZR59Ap5Fes7QuDbDgTj9 	</li> 	<br/>
+<li>	Namalyongo, A., Achón, Z. y Ábalo, R. (2013). Factores de riesgo y vulnerabilidad al estrés en estudiantes universitarios. Psicogente, 16(29), 143-154. http://revistas.unisimon.edu.co/index.php/psicogente/article/view/1947 	</li> 	<br/>
+<li>	Nezu, A., Maguth ,C. y D’Zurilla, T. (2014). Terapia de solución de problemas. Editorial Desclée de Brouwer.	</li> 	<br/>
+<li>	Oman, D., Shapiro, S. L., Thoresen, C. E., Plante, T. G. y Flinders, T. (2008). Meditation lowers stress and supports forgiveness among college students: A randomized controlled trial. Journal of American College Health, 56(5), 569-578. https://doi.org/10.3200/jach.56.5.569-578 	</li> 	<br/>
+<li>	Pereira, M. (2009). Una revisión teórica sobre el estrés y algunos aspectos relevantes de éste en el ámbito educativo. Revista Educación, 33(2), 171-190.	</li> 	<br/>
+<li>	Pérez, J., García, R. y Pérez F. (s.f.). Guía para el manejo del estrés académico. https://www.uv.es/iqdocent/guias/estres.pdf 	</li> 	<br/>
+<li>	Santos, A., Fernández, M., Reig, A., Riquelme, L., Montero, E. y Peralta, M. (2019). Valoración del estrés percibido y de las necesidades y demandas de intervención psicoeducativa para su manejo eficaz en estudiantes universitarios. En R. Roig-Vila (Ed.), Investigación e innovación en la Enseñanza Superior. Nuevos contextos, nuevas ideas (pp. 409-416). Editorial Octaedro.	</li> 	<br/>
+<li>	Shapiro, S.L., Brown, K.W. y Biegel, G.M. (2007). Teaching self-care to caregivers: Effects of mindfulness based stress reduction on the mental health of therapists in training [Efectos de la reducción del estrés basada en la atención plena en la salud mental de los terapeutas en formación]. Training and Education in Professional Psychology, 1(2), 105-115. https://psycnet.apa.org/doi/10.1037/1931-3918.1.2.105 	</li> 	<br/>
+<li>	Tenelanda, D., Castelo, M., Cando, I., Fierro, S. y Asqui, J. (2017). Las técnicas de respiración, relajación y visualización para el manejo del estrés académico en el proceso de aprendizaje del idioma inglés. https://eventos.uho.edu.cu/index.php/ccm/cci8/paper/viewPaper/1952 	</li> 	<br/>
+<li>	Treviño, J. y Ramírez, M. (2012). Estrategias cognitivo-conductuales para el manejo del estrés en alumnos mexicanos de bachillerato internacional. Alternativas en Psicología, 16(26), 26-38.	</li> 	<br/>
+<li>	Valenzuela, M., Gallegos, L., Baca, L., López, H. y Rico, F. (2021). Estrés académico en universitarios y la práctica de ejercicio físico-deportivo. Revista Publicando, 8(28), 1-8. https://doi.org/10.51528/rp.vol8.id2175 	</li> 	<br/>
+<li>	Valverde, C. V., López, M. C. y Ring, J. M. (2003). Estrategias de afrontamiento. En A. Bulbena Vilarrasa, G. E. Barrios y P. F. De Larrinoa Palacios (Eds.), Medicina Clínica en Psiquiatría y Psicología (425-435). Editorial Masson.	</li> 	<br/>
+<li>	Villarroel, A. (2015). Intervención cognitivo-conductual y centrada en soluciones para disminuir el estrés académico en estudiantes universitarios [Tesis de maestría, Universidad Autónoma de Nuevo León]. Recursos Digitales Abiertos (REDIAB) de la Universidad Autónoma de Nuevo León. http://eprints.uanl.mx/9644/1/1080215011.pdf	</li> 	<br/>
+<li>	Villen, J. (2016). Técnicas cognitivo-conductuales para afrontar el estrés de los exámenes. https://www.elsevier.com/es-es/connect/estudiantes-de-ciencias-de-la-salud/tecnicas-cognitivo-conductuales-para-afrontar-el-estres-de-los-examenes 	</li> 	<br/>
+<li>	Zuazua, A. L. V. y Ramírez, M. T. G. (2017). Intervención cognitivo-conductual y centrada en soluciones para disminuir el estrés académico en estudiantes universitarios. Revista Electrónica de Psicología Iztacala, 18(4), 1363-1387. http://www.revistas.unam.mx/index.php/repi/article/view/53434/47526 	</li> 
+
 
                         </ol>
                         `} title={'Referencia'} />
