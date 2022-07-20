170c816cd1da110da0fb6f7ac94cb19723777ee2
miguepoloc <miguepoloc@gmail.com>
2022-07-19 20:46:06 -0500

    Corrección de cita en modo cursiva

--- src/components/Dashboard/mod_estres/ActivityCard.jsx
+++ src/components/Dashboard/mod_estres/ActivityCard.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import { FaLeaf } from 'react-icons/fa'
-export const ActivityCard = ({title,desc,_class,url}) => {
+export const ActivityCard = ({ title, desc, _class, url }) => {
 
-  return (
-    <div className={"activityCard-card d-flex flex-column " + (_class? _class:"")} style={{cursor: "auto"}}>
-      <div className="activityCard-card-header ">
-                        <div style={{backgroundColor:"#F5F7FE"}} className="rounded-1"><FaLeaf className='p-1' size={35} fill={"#B2C7FC"}/></div>
-                     
-                    </div>
-    <div className="activityCard-card-title">{title}</div>
-    <div className="activityCard-card-subtitle">
-        {desc}
-    </div>
-    {/* TODO: Desactivar esto cuando la plataforma no esté en pilotaje */}
-    {/* <div className="activityCard-card-buttons">
+    return (
+        <div className={"activityCard-card d-flex flex-column " + (_class ? _class : "")} style={{ cursor: "auto" }}>
+            <div className="activityCard-card-header ">
+                <div style={{ backgroundColor: "#F5F7FE" }} className="rounded-1"><FaLeaf className='p-1' size={35} fill={"#B2C7FC"} /></div>
+
+            </div>
+            <div className="activityCard-card-title">{title}</div>
+            <div className="activityCard-card-subtitle" dangerouslySetInnerHTML={{ __html: desc }}>
+            </div>
+            {/* TODO: Desactivar esto cuando la plataforma no esté en pilotaje */}
+            {/* <div className="activityCard-card-buttons">
       <a href={"/"+url}>
         <button className="search-buttons card-buttons">Ver actividad</button>
         </a>
     </div> */}
-</div>
-  )
+        </div>
+    )
 }
--- src/components/Dashboard/mod_estres/SeccionInicial.jsx
+++ src/components/Dashboard/mod_estres/SeccionInicial.jsx
@@ -1,4 +1,4 @@
-import React,{render} from 'react'
+import React from 'react'
 import { renderToString } from "react-dom/server";
 import { Actividad } from "../Actividad"
 import { Descripcion } from '../Descripcion'
@@ -6,12 +6,11 @@ import { imgGanso } from '../../../helpers/helper_imagen_ganso'
 import { FcSurvey } from 'react-icons/fc'
 import { ActivityCards } from './ActivityCards'
 import ButtonLibro from '../ButtonLibro'
-import { FaBolt, FaBookOpen } from 'react-icons/fa'
-import { Tip } from '../Tip';
+import { FaBookOpen } from 'react-icons/fa'
 export const SeccionInicial = () => {
     return (
         <>
-        
+
             <Actividad src={imgGanso.meditando} title="<h3 class='d-flex align-items-center justify-content-center w-100 mb-0'>Bienvenido al Módulo Relax</h3> <hr class='mt-1 mb-2' /> ¿Para qué me servirá este módulo?"
                 text={`<ul class="ms-2">
     <li>Reconocer el concepto de estrés.</li>
@@ -19,15 +18,15 @@ export const SeccionInicial = () => {
     <li>Desarrollar habilidades enfocadas al manejo adecuado del estrés propio o ajeno en situaciones cotidianas.</li>
     </ul>
     <small class="d-flex justify-content-center"><span style="color:red">*</span>${renderToString(<div className='mx-1' >
-            
-            <div
-                className={" buttonReferenciaNoFixed text-white"}   style={{cursor: "auto"}} 
-            >
-                <FaBookOpen size={14} />
-            </div>
-            <ButtonLibro asImg={true} size={14} />
 
-        </div>)} Aquí podrás encontrar referencias bibliográficas del contenido de este módulo  e información adicional sobre el tema de estrés.</small>`} showIcon={false} />
+                    <div
+                        className={" buttonReferenciaNoFixed text-white"} style={{ cursor: "auto" }}
+                    >
+                        <FaBookOpen size={14} />
+                    </div>
+                    <ButtonLibro asImg={true} size={14} />
+
+                </div>)} Aquí podrás encontrar referencias bibliográficas del contenido de este módulo  e información adicional sobre el tema de estrés.</small>`} showIcon={false} />
 
             <Actividad src={imgGanso.elegante} title="¿Qué es el estrés?"
                 text={`Es la sensación de tensión que se origina en nosotros cuando consideramos que una determinada situación se sale de nuestro control y nos obliga a recurrir a nuevas estrategias o habilidades, dando la idea de que será imposible enfrentar a la misma, puede ocurrir con muchos casos de nuestra cotidianidad (Ávila, 2014).`} showIcon={false} />
@@ -58,56 +57,56 @@ export const SeccionInicial = () => {
 
                 <h4 className='text-center mb-2 '> Algunos de sus signos </h4>
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Aceleración del ritmo cardiaco
                 </button>
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Temblores
                 </button>
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Dolor de cabeza
                 </button>
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Respiración agitada.
                 </button>
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Malestares digestivos.
                 </button>
 
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Irritabilidad.
                 </button>
 
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Desorden en hábitos saludables.
                 </button>
 
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Frustración.
                 </button>
 
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Pensamientos de incapacidad.
                 </button>
 
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Inadaptabilidad al cambio.
                 </button>
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Problemas de concentración.
                 </button>
 
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Poca agilidad para la realización de tareas.
                 </button>
-                <button className="search-buttons rounded-pill mx-1" style={{cursor: "auto"}}>
+                <button className="search-buttons rounded-pill mx-1" style={{ cursor: "auto" }}>
                     Tristeza
                 </button>
             </div>
@@ -125,7 +124,7 @@ export const SeccionInicial = () => {
 
             <ActivityCards />
 
-            <Actividad  showIcon={false} src={imgGanso.explicando} title={"¿Cómo empiezo?"} color={"#A1B7EF"} text={"<span class='d-flex justify-content-center'>¡Puedes hacerlo dandole click al boton siguiente!</span>"} />
+            <Actividad showIcon={false} src={imgGanso.explicando} title={"¿Cómo empiezo?"} color={"#A1B7EF"} text={"<span class='d-flex justify-content-center'>¡Puedes hacerlo dandole click al boton siguiente!</span>"} />
 
         </>
 
